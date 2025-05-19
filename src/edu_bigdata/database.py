52288d19cdfbc3be27c6b70d5b00df0cb08a5744
Carlos Arturo Gómez Jiménez <carlos.gomez@wedoengineer.com>
2025-05-18 22:30:03 -0500

    creacion bd

--- src/edu_bigdata/database.py
+++ src/edu_bigdata/database.py
@@ -9,13 +9,37 @@ class DataBase:
         
         self.create_database()
     
-    def create_database(self):
+    # def create_database(self):
+    #     try:
+    #         self.conn = sqlite3.connect(self.db_name)
+    #         self.cursor = self.conn.cursor()
+    #         self.conn.close()
+            
+    #     except Exception as err:
+    #         print("Error al crear la base de datos")
+    #         # df.sql(self.conn, nom_tabla)
+    
+    #CRUD  
+    def insert_data(self,df = pd.DataFrame(),nom_table="minas_analisis"):
         try:
-            self.conn = sqlite3.connect(self.db_name)
-            self.cursor = self.conn.cursor()
-            
-            
-        except Exception as err:
-            print("Error al crear la base de datos")
-            # df.sql(self.conn, nom_tabla)
-            
+            df = df.copy()
+            conn = sqlite3.connect(self.db_name)
+            df.to_sql(name=nom_table,con=conn,if_exists='replace')
+            conn.close()
+        except Exception as errores:
+            print("error al guradar los datos")
+    
+    def read_data(self,nom_table=""):
+        df=pd.DataFrame()
+        try:
+            if len(nom_table)>0:
+                conn = sqlite3.connect(self.db_name)
+                query= "select * from {}".format(nom_table)
+                df = pd.read_sql_query(sql=query,con=conn)
+                print("*************** consulta base datos tabla: {}*********".format(query))
+                conn.close
+                return df
+        except Exception as errores:
+            print("error al obtener los datos")
+            return df
+    
--- src/edu_bigdata/main.py
+++ src/edu_bigdata/main.py
@@ -10,7 +10,10 @@ def main():
     dw.convertir_numericos()
     dw.exportar_csv()
     dw.df.to_csv("src/edu_bigdata/static/csv/data_web.csv", index=False)
-    
+    nombre_tabla = "minas_analisis"
+    database.insert_data(dw,nombre_tabla)
+    df_2 = database.read_data(nombre_tabla)
+    print(df_2.head())
     
 
 if __name__ == "__main__":

689f717702eb3e3128e36f03af57157cbd06520a
Carlos Arturo Gómez Jiménez <carlos.gomez@wedoengineer.com>
2025-05-18 20:33:29 -0500

    actualizar

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "edu_bigdata",
+    autor = "Carlos Arturo Gómez Jiménez",
+    author_email = "cargomez-95@hotmail.com",
+    description = "",
+    install_requires = [
+        "pandas",
+        "openpyxl",
+        "requests",
+    ]
+)
